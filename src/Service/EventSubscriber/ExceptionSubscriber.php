ba0de598e4695f084fdeeb520d53201968f82894
bartek3xe <bartek3xe@gmail.com>
2024-10-31 22:19:48 +0100

    Prepare error pages

---
+++ src/Service/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private readonly Environment $twig)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        $statusCode = $exception instanceof HttpExceptionInterface
+            ? $exception->getStatusCode()
+            : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        if ($statusCode === Response::HTTP_NOT_FOUND) {
+            $content = $this->twig->render('_exception/error404.html.twig');
+        } else {
+            $content = $this->twig->render('_exception/error.html.twig', [
+                'status_code' => $statusCode,
+                'status_text' => Response::$statusTexts[$statusCode] ?? 'Error',
+            ]);
+        }
+
+        $response = new Response($content, $statusCode);
+        $event->setResponse($response);
+    }
+}
