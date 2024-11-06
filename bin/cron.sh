67918c613fd4fb7c0bd30f2bfe736ecd28ea1542
bartek3xe <bartek3xe@gmail.com>
2024-11-06 12:19:43 +0100

    Install symfony flex and fix cron.sh

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if pgrep -f "messenger:consume"
+if pgrep -f "messenger:consume redis_async" > /dev/null
 then
     echo "Command already running."
 else
     echo "Starting command"
-    /opt/alt/php82/usr/bin/php /home/srv51178/domains/tediefootwear.com/public_html/bin/console messenger:consume async -vv
+/opt/alt/php82/usr/bin/php /home/srv51178/domains/banodev.pl/public_html/bin/console messenger:consume redis_async -vv --env=prod --time-limit=3600 --memory-limit=128M
 fi
